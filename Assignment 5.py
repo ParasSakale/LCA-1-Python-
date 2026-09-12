89ab9e233b40d879e47c235fd92d3ab32f668167
Paras_Sakale_2008 <parassakale2008@gmail.com>
2026-09-12 13:55:52 +0530

    Add files via upload

---
+++ Assignment 5.py
@@ -0,0 +1,31 @@
+#Python program to check that a string contains only a certain set of characters
+# (in this case a-z, A-Z and 0-9).
+
+#Fancy details
+print("=" * 50)
+print("Welcome to the String checker")
+print("=" * 50)
+
+#Actual code
+while True:
+    input_string = input("Type your string : ")
+
+    is_valid = True
+    for character in input_string:
+        if not character.isalnum():
+            is_valid = False
+            break
+
+    if is_valid:
+        print("The string contains only characters (a-z , A-Z) and digits (0-9)")
+        print("Hence it is a string")
+    else:
+        print("The string contains characters outside a-z, A-Z, 0-9")
+        print("Hence it is not a string")
+
+    check_another_string = input("Want to check for another string (y/n) : ")
+    if check_another_string != "y":
+        print("=" * 50)
+        print("Thank you for using this program")
+        print("=" * 50)
+        break
